74637f45975f9d5b23769220bd668c16af716a5e
Taras Verhun <tarassel@gmail.com>
2013-10-11 20:18:49 +0300

    IsAnnagram2

--- 1.1/1.cpp
+++ 1.1/1.cpp
@@ -6,6 +6,8 @@
 #include <cstring>
 #include <string>
 #include <algorithm>
+#include <map>
+#include <functional>
 
 bool IsThereDuplicates(char* str, int nLen)
 {
@@ -42,12 +44,35 @@ bool IsAnnagram1(std::string& str1, std::string& str2)
 	return str1 == str2;
 }
 
+/*template<class _TFreqTable, class _TElem>
+struct ModyFreqTable : public std::binary_function<_TFreqTable, _TElem, bool>
+{
+	bool operator()(_TFreqTable& freqTable, _TElem element) const
+	{
+		//freqTable.insert(std::make_pair(element, ++freqTable[element]));
+		freqTable[element] = freqTable[element] + 1;
+	}
+};*/
+class ModyFreqTable
+{
+	std::map<char, int>& _freq;
+public:
+	ModyFreqTable(std::map<char, int>& freq): _freq(freq) {}
+	void operator()(char c)
+	{
+		_freq[c] = _freq[c] + 1;
+	}
+};
+
 bool IsAnnagram2(std::string& str1, std::string& str2)
 {
-	std::sort(str1.begin(), str1.end());
-	std::sort(str2.begin(), str2.end());
+	std::map<char, int> freq1;
+	std::map<char, int> freq2;
+	
+	std::for_each(str1.cbegin(), str1.cend(), ModyFreqTable(freq1));
+	std::for_each(str2.cbegin(), str2.cend(), ModyFreqTable(freq2));
 
-	return str1 == str2;
+	return freq1 == freq2;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -75,6 +100,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	assert(IsAnnagram1(cstr1, cstr2) == true);
 	assert(IsAnnagram1(cstr1, cstr3) == false);
 
+	cstr1 = "qwerty";
+	cstr2 = "ewqtry";
+	cstr3 = "wQeete";
 	assert(IsAnnagram2(cstr1, cstr2) == true);
 	assert(IsAnnagram2(cstr1, cstr3) == false);
 
